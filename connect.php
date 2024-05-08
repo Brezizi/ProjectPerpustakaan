ab6d9d01b58b759e1135e697125aebc30d5314d8
Brezizi <brezizi.id@gmail.com>
2024-05-08 10:11:13 +0700

    koneksi

--- connect.php
+++ connect.php
@@ -1,5 +1,5 @@
 <?php
-    $conn=pg_connect("host=localhost port=5432 dbname=perpus user=postgres password=");
+    $conn=pg_connect("host=localhost port=5432 dbname=perpustakaan user=postgres password=");
     /* check connection */
     if (!$conn) {
         echo "<h1>Not Connected</h1>";
