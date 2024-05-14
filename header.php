6fff20de9391480f6bb2cc9f1c739755a5b31bf1
Brezizi <brezizi.id@gmail.com>
2024-05-14 08:32:38 +0700

    update home ui

--- header.php
+++ header.php
@@ -16,10 +16,10 @@
                 <img class="w-16" src="./image-src/ts-mark.png" alt="logo">
             <div class="hidden w-full md:block md:w-auto" id="navbar-default">
                 <ul class="font-medium flex flex-col p-3 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-100 md:flex-row md:space-x-4 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-gray-100">
-                  <li><a href="./siswa/add_student.php" class="block md:py-3 md:px-5 text-gray-900 rounded-md hover:py-3 hover:px-5 md:hover:bg-gray-100 md:hover:bg-gray-800 md:border-0 md:hover:text-gray-100 md:p-0 transition delay-5s">Add Student</a></li>
-                  <li><a href="./petugas/add_employee.php" class="block md:py-3 md:px-5 text-gray-900 rounded-md hover:py-3 hover:px-5 md:hover:bg-gray-100 md:hover:bg-gray-800 md:border-0 md:hover:text-gray-100 md:p-0 transition delay-5s">Add Employee</a></li>
-                  <li><a href="./buku/add_book.php" class="block md:py-3 md:px-5 text-gray-900 rounded-md hover:py-3 hover:px-5 md:hover:bg-gray-100 md:hover:bg-gray-800 md:border-0 md:hover:text-gray-100 md:p-0 transition delay-5s">Add Book</a></li>
-                  <li><a href="student_data.php" class="block md:py-3 md:px-5 text-gray-900 rounded-md hover:py-3 hover:px-5 md:hover:bg-gray-100 md:hover:bg-gray-800 md:border-0 md:hover:text-gray-100 md:p-0 transition delay-5s">Student Data</a></li>
+                  <li><a href="./siswa/add_student.php" class="font-regular block md:py-3 md:px-5 text-gray-900 rounded-md hover:font-bold hover:py-3 hover:px-5 md:hover:bg-gray-100 md:hover:bg-red-700 md:border-0 md:hover:text-gray-100 md:p-0 transition delay-5s">Add Student</a></li>
+                  <li><a href="./petugas/add_employee.php" class="font-regular block md:py-3 md:px-5 text-gray-900 rounded-md hover:font-bold hover:py-3 hover:px-5 md:hover:bg-gray-100 md:hover:bg-red-700 md:border-0 md:hover:text-gray-100 md:p-0 transition delay-5s">Add Employee</a></li>
+                  <li><a href="./buku/add_book.php" class="font-regular block md:py-3 md:px-5 text-gray-900 rounded-md hover:font-bold hover:py-3 hover:px-5 md:hover:bg-gray-100 md:hover:bg-red-700 md:border-0 md:hover:text-gray-100 md:p-0 transition delay-5s">Add Book</a></li>
+                  <li><a href="student_data.php" class="font-regular block md:py-3 md:px-5 text-gray-900 rounded-md hover:font-bold hover:py-3 hover:px-5 md:hover:bg-gray-100 md:hover:bg-red-700 md:border-0 md:hover:text-gray-100 md:p-0 transition delay-5s">Student Data</a></li>
                 </ul>
               </div>
             </div>
